836bbd3e6adb581d5839fb3c246e4a9ef9ea9979
KoEkko <2251930460@qq.com>
2023-06-20 15:31:52 +0800

    feat:1.demo

--- 代码/1.利用脚手架创建一个项目/demo/src/index.js
+++ 代码/1.利用脚手架创建一个项目/demo/src/index.js
@@ -1,9 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+/* 对ES6内置API做兼容处理 */
+import "react-app-polyfill/ie9";
+import "react-app-polyfill/stable";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "@/index.less";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <div>React Learning</div>
   </React.StrictMode>
 );
+
+fetch("/jian/subscriptions/recommended_collections")
+  .then((res) => res.json())
+  .then((res) => {
+    console.log(res);
+  });
+
+fetch("zhi/news/latest")
+  .then((res) => res.json())
+  .then((res) => {
+    console.log(res);
+  });
